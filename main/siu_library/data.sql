96cfeb1b7aee06823786fc6e48555fe6069a60e0
ankitpati <ankitpati@gmail.com>
2016-08-10 09:57:27 +0530

    schema updated, data updated to match

--- main/siu_library/data.sql
+++ main/siu_library/data.sql
@@ -5,69 +5,81 @@
 
 use siu_library;
 
-insert into authors (name) values("Brian Kernighan");
-insert into authors (name) values("Dennis Ritchie");
-insert into authors (name) values("Ken Thompson");
-insert into authors (name) values("Mike Stone");
-insert into authors (name) values("P. B. Shelley");
+insert into library (lname, city) values("SIT", "Pune");
+insert into library (lname, city) values("SSBS", "Pune");
+insert into library (lname, city) values("SSCA", "Kolkata");
+insert into library (lname, city) values("SIBM", "Hyderabad");
+insert into library (lname, city) values("SLS", "Bhubaneswar");
 
-insert into department (name) values("Computer Science");
-insert into department (name) values("Information Technology");
-insert into department (name) values("Electronics and Telecommunication");
-insert into department (name) values("Mechanical Engineering");
-insert into department (name) values("Civil Engineering");
+insert into books values(1111, "The C Programming Language", 2, 1);
+insert into books values(2222, "The C++ Programming Language", 3, 1);
+insert into books values(3333, "Law of Torts", 4, 5);
+insert into books values(4444, "The Unix Philosophy", 1, 1);
+insert into books values(5555, "The Modern Prometheus", 5, 4);
 
-insert into instlib (name, location) values("Symbiosis Institute of Technology", "Pune");
-insert into instlib (name, location) values("Symbiosis School of Biomedical Sciences", "Pune");
-insert into instlib (name, location) values("Symbiosis School of Culinary Arts", "Kolkata");
-insert into instlib (name, location) values("Symbiosis Institute of Business Management", "Hyderabad");
-insert into instlib (name, location) values("Symbiosis Law School", "Bhubaneswar");
+insert into author (aname) values("Brian Kernighan");
+insert into author (aname) values("Dennis Ritchie");
+insert into author (aname) values("Ken Thompson");
+insert into author (aname) values("Mike Stone");
+insert into author (aname) values("P. B. Shelley");
 
-insert into sellers (location, contact) values("Kolkata", "+919999988888");
-insert into sellers (location, contact) values("Pune", "+918888899999");
-insert into sellers (location, contact) values("Hyderabad", "+918888888888");
-insert into sellers (location, contact) values("Bhubaneswar", "+919999999999");
-insert into sellers (location, contact) values("Cuttack", "+919999911111");
+insert into publisher (pname) values("Tata McGraw Hill");
+insert into publisher (pname) values("Prentice Hall");
+insert into publisher (pname) values("Nirali Prakashan");
+insert into publisher (pname) values("Hello World");
+insert into publisher (pname) values("Sunshine");
 
-insert into books values(1111, "The C Programming Language", 2);
-insert into books values(2222, "The C++ Programming Language", 3);
-insert into books values(3333, "Law of Torts", 4);
-insert into books values(4444, "The Unix Philosophy", 1);
-insert into books values(5555, "The Modern Prometheus", 5);
+insert into seller (slname, city) values("National Book Store", "Kolkata");
+insert into seller (slname, city) values("College Book Store", "Pune");
+insert into seller (slname, city) values("Crossword", "Hyderabad");
+insert into seller (slname, city) values("City Book Store", "Bhubaneswar");
+insert into seller (slname, city) values("Golden Books", "Cuttack");
 
-insert into buys_from values(1, 3);
-insert into buys_from values(1, 2);
-insert into buys_from values(2, 5);
-insert into buys_from values(2, 1);
-insert into buys_from values(3, 3);
-insert into buys_from values(3, 1);
-insert into buys_from values(4, 2);
-insert into buys_from values(4, 1);
-insert into buys_from values(5, 4);
-insert into buys_from values(5, 3);
+insert into books_by values(1111, 3, 4);
+insert into books_by values(1111, 2, 2);
+insert into books_by values(2222, 5, 5);
+insert into books_by values(2222, 1, 1);
+insert into books_by values(3333, 3, 4);
+insert into books_by values(3333, 1, 1);
+insert into books_by values(4444, 2, 2);
+insert into books_by values(4444, 1, 2);
+insert into books_by values(5555, 4, 3);
+insert into books_by values(5555, 3, 3);
 
-insert into members (name, institute_id, department_id) values("Ankit Pati", 1, 2);
-insert into members (name, institute_id, department_id) values("Tiashaa Chatterjee", 5, 5);
-insert into members (name, institute_id, department_id) values("Mayank Verma", 2, 1);
-insert into members (name, institute_id, department_id) values("Sahil Pandey", 4, 3);
-insert into members (name, institute_id, department_id) values("Ravi Tripathi", 3, 1);
+insert into department (deptname, institutename) values("Information Technology", "SIT");
+insert into department (deptname, institutename) values("Computer Science", "SIT");
+insert into department (deptname, institutename) values("Nutrition", "SSBS");
+insert into department (deptname, institutename) values("Medicine", "SSBS");
+insert into department (deptname, institutename) values("Torts", "SLS");
+insert into department (deptname, institutename) values("Common Law", "SLS");
+insert into department (deptname, institutename) values("Cuisine", "SSCA");
+insert into department (deptname, institutename) values("Nutrition", "SSCA");
+insert into department (deptname, institutename) values("Accounting", "SIBM");
+insert into department (deptname, institutename) values("Communication", "SIBM");
 
-insert into owns values(1, 2222, 4);
-insert into owns values(1, 3333, 9);
-insert into owns values(2, 4444, 2);
-insert into owns values(2, 3333, 5);
-insert into owns values(3, 5555, 6);
-insert into owns values(3, 2222, 7);
-insert into owns values(4, 3333, 1);
-insert into owns values(4, 2222, 7);
-insert into owns values(5, 2222, 9);
-insert into owns values(5, 4444, 8);
+insert into student values(543, "Tiashaa Chatterjee", 6);
+insert into student values(506, "Ankit Pati", 1);
+insert into student values(412, "Sahil Pandey", 9);
+insert into student values(125, "Ravi Tripathi", 5);
+insert into student values(445, "Mayank Verma", 4);
 
-insert into publishers (isbn, name) values(2222, "Tata McGraw Hill");
-insert into publishers (isbn, name) values(1111, "Prentice Hall");
-insert into publishers (isbn, name) values(3333, "Nirali Prakashan");
-insert into publishers (isbn, name) values(5555, "Hello World");
-insert into publishers (isbn, name) values(4444, "Sunshine");
+insert into staff values(14, "Pradeep Bhattacharya", 6);
+insert into staff values(43, "Subho Das", 1);
+insert into staff values(42, "Sanjay Basak", 9);
+insert into staff values(48, "Ahmed Raza", 8);
+insert into staff values(79, "Chandan Chatterjee", 7);
+
+insert into purchase (lid, slid, pid, bid, month) values(1, 2, 3, 4444, 12);
+insert into purchase (lid, slid, pid, bid, month) values(4, 3, 2, 1111, 4);
+insert into purchase (lid, slid, pid, bid, month) values(4, 1, 2, 3333, 1);
+insert into purchase (lid, slid, pid, bid, month) values(5, 1, 2, 4444, 11);
+insert into purchase (lid, slid, pid, bid, month) values(2, 5, 3, 4444, 5);
+
+insert into issue (memid, bid, lid, issuedate, returndate) values(4, 4444, 1, "1995-12-13", "1996-04-02");
+insert into issue (memid, bid, lid, issuedate, returndate) values(1, 1111, 4, "1996-12-13", "1997-04-02");
+insert into issue (memid, bid, lid, issuedate, returndate) values(2, 3333, 4, "1997-12-13", "1998-04-02");
+insert into issue (memid, bid, lid, issuedate, returndate) values(5, 4444, 5, "1998-12-13", "1999-04-02");
+insert into issue (memid, bid, lid, issuedate, returndate) values(3, 4444, 2, "1999-12-13", "2000-04-02");
 
 insert into sells values(1, 3);
 insert into sells values(1, 2);
@@ -79,11 +91,4 @@ insert into sells values(4, 4);
 insert into sells values(4, 3);
 insert into sells values(5, 3);
 insert into sells values(5, 2);
-
-insert into staff values(3, "Librarian", 50000);
-insert into staff values(4, "Professor", 200000);
-
-insert into student values(1, 3.5, 3);
-insert into student values(2, 3.9, 3);
-insert into student values(5, 3.2, 2);
 /* end of siu_library/schema.sql */
--- main/siu_library/schema.sql
+++ main/siu_library/schema.sql
@@ -3,86 +3,100 @@
  * Author: Ankit Pati
  */
 
+drop database siu_library;
 create database siu_library;
 use siu_library;
 
-create table authors(
-    author_id integer primary key not null auto_increment,
-    name char(100) not null
-);
-
-create table department(
-    id integer primary key not null auto_increment,
-    name char(100) unique not null
-);
-
-create table instlib(
-    id integer primary key not null auto_increment,
-    name char(100) not null,
-    location char(250) not null
-);
-
-create table sellers(
-    id integer primary key not null auto_increment,
-    location char(250) not null,
-    contact char(13) unique not null
+create table library(
+    lid integer primary key not null auto_increment,
+    lname char(100) not null,
+    city char(100) not null
 );
 
 create table books(
-    isbn integer primary key not null,
-    name char(100) not null,
-    author_id integer not null,
-    foreign key(author_id) references authors(author_id)
+    bid integer primary key not null,
+    bname char(100) not null,
+    totalcopies integer not null,
+    lid integer not null,
+    foreign key(lid) references library(lid)
 );
 
-create table buys_from(
-    institute_id integer not null,
-    seller_id integer not null,
-    foreign key(institute_id) references instlib(id)
+create table author(
+    aid integer primary key not null auto_increment,
+    aname char(100) not null
 );
 
-create table members(
-    id integer primary key not null auto_increment,
-    name char(100) not null,
-    institute_id integer not null,
-    department_id integer not null,
-    foreign key(institute_id) references instlib(id),
-    foreign key(department_id) references department(id)
+create table publisher(
+    pid integer primary key not null auto_increment,
+    pname char(100) not null
 );
 
-create table owns(
-    institute_id integer not null,
-    isbn integer not null,
-    copies integer not null,
-    foreign key(institute_id) references instlib(id),
-    primary key(institute_id, isbn)
+create table seller(
+    slid integer primary key not null auto_increment,
+    slname char(100) not null,
+    city char(100) not null
 );
 
-create table publishers(
-    id integer primary key not null auto_increment,
-    isbn integer not null,
-    name char(100) not null,
-    foreign key(isbn) references books(isbn)
+create table books_by(
+    bid integer not null,
+    aid integer not null,
+    pid integer not null,
+    unique(bid, aid, pid),
+    foreign key(bid) references books(bid),
+    foreign key(aid) references author(aid),
+    foreign key(pid) references publisher(pid)
 );
 
-create table sells(
-    seller_id integer not null,
-    publisher_id integer not null,
-    foreign key(seller_id) references sellers(id),
-    foreign key(publisher_id) references publishers(id)
-);
-
-create table staff(
-    member_id integer primary key not null,
-    post char(100) not null,
-    salary integer not null,
-    foreign key(member_id) references members(id)
+create table department(
+    deptid integer primary key not null auto_increment,
+    deptname char(100) not null,
+    institutename char(100) not null,
+    unique(deptname, institutename)
 );
 
 create table student(
-    member_id integer primary key not null,
-    gpa float not null,
-    yr year not null,
-    foreign key(member_id) references members(id)
+    sid integer primary key not null,
+    sname char(100) not null,
+    deptid integer not null,
+    foreign key(deptid) references department(deptid)
+);
+
+create table staff(
+    stid integer primary key not null,
+    stname char(100) not null,
+    deptid integer not null,
+    foreign key(deptid) references department(deptid)
+);
+
+create table purchase(
+    prid integer primary key not null auto_increment,
+    lid integer not null,
+    slid integer not null,
+    pid integer not null,
+    bid integer not null,
+    month integer not null,
+    foreign key(lid) references library(lid),
+    foreign key(slid) references seller(slid),
+    foreign key(pid) references publisher(pid),
+    foreign key(bid) references books(bid)
+);
+
+create table issue(
+    issueid integer primary key not null auto_increment,
+    memid integer not null,
+    bid integer not null,
+    lid integer not null,
+    issuedate date not null,
+    returndate date not null,
+    foreign key(bid) references books(bid),
+    foreign key(lid) references library(lid)
+);
+
+create table sells(
+    slid integer not null,
+    pid integer not null,
+    unique(slid, pid),
+    foreign key(slid) references seller(slid),
+    foreign key(pid) references publisher(pid)
 );
 /* end of siu_library/schema.sql */
