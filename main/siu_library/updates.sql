f89762a8b07c0952c764c0ecda81065b0bbe5a5b
ankitpati <ankitpati@gmail.com>
2016-08-24 10:34:15 +0530

    updates added

---
+++ main/siu_library/updates.sql
@@ -0,0 +1,29 @@
+/* siu_library/updates.sql */
+/* Date  : 24 August 2016
+ * Author: Ankit Pati
+ */
+
+update books
+set price = price * 11
+where bid = 5555;
+
+update seller
+set city = "Nagpur"
+where slname = "City Book Store";
+
+update publisher natural join books_by natural join books
+set price = price + 100
+where pname = "Nirali Prakashan";
+
+update books natural join books_by natural join author
+set totalcopies = totalcopies - 2
+where aname = "Mike Stone";
+
+update department
+set deptname = "CS"
+where institutename = "SIT" and deptname = "Computer Science";
+
+update department natural join student
+set sname = "Mayank"
+where institutename = "SSBS" and deptname = "Medicine";
+/* end of siu_library/updates.sql */
