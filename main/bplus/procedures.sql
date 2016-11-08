135aaef2e812c1417c6d91cb95b0df4a12d24704
ankitpati <ankitpati@gmail.com>
2016-11-08 09:39:35 +0530

    bplus: demit and close procedures, added queries

--- main/bplus/procedures.sql
+++ main/bplus/procedures.sql
@@ -6,26 +6,44 @@
 use bplus;
 
 delimiter $
+create procedure demit_patient(in person_id integer unsigned)
+begin
+    delete from patient where patient.person_id = person_id;
+end $
+delimiter ;
+
+delimiter $
 create procedure admit_patient(in person_id integer unsigned,
     in hospital_registration_number integer unsigned,
     in doctor_id integer unsigned)
 begin
     start transaction;
+        call demit_patient(person_id);
         insert into patient values(person_id, hospital_registration_number,
             doctor_id);
     commit;
 end $
 delimiter ;
-call admit_patient(9, 1, 7);
+
+delimiter $
+create procedure close_request(in patient_id integer unsigned)
+begin
+    delete from request where request.patient_id = patient_id;
+end $
+delimiter ;
 
 delimiter $
 create procedure make_request(in patient_id integer unsigned,
     in volume integer unsigned)
 begin
     start transaction;
+        call close_request(patient_id);
         insert into request values(patient_id, volume);
     commit;
 end $
 delimiter ;
+
+call admit_patient(9, 1, 7);
 call make_request(9, 2);
+call make_request(2, 4);
 /* end of bplus/procedures.sql */
---
+++ main/bplus/queries.sql
@@ -0,0 +1,27 @@
+/* bplus/queries.sql */
+/* Date  : 06 November 2016
+ * Author: Ankit Pati
+ */
+
+use bplus;
+
+select name as "Name", phone_number as "Phone Number"
+from person natural join phone;
+
+select person.name as "Dr", hospital.name as "Hospital"
+from person natural join doctor
+    inner join hospital on(registration_number = hospital_registration_number);
+
+select sum(volume) as "Total blood requested"
+from request;
+
+select sum(volume) as "Total blood requested by Ankit"
+from request_log inner join person on(patient_id = person_id)
+where name = "Ankit Pati"
+group by patient_id;
+
+select latitude, longitude
+from patient
+    inner join hospital on(registration_number = hospital_registration_number)
+where person_id = 2;
+/* end of bplus/queries.sql */
